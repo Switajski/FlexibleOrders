488d5164014d52572c3d079e8313f89953862440
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.Product --activeRecord false --testAutomatically

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/ProductDataOnDemand.java
@@ -0,0 +1,6 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Product.class)
+public class ProductDataOnDemand {
+}
---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/ProductIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Product.class)
+public class ProductIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}

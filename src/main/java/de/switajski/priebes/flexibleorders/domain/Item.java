286be5e32a35a8138a050fbc34ef3d0cb16aae43
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field reference --fieldName product --type ~.domain.Product --notNull --cardinality ONE_TO_ONE --class ~.domain.Item

--- src/main/java/de/switajski/priebes/flexibleorders/domain/Item.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/Item.java
@@ -2,9 +2,17 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity(inheritanceType = "TABLE_PER_CLASS")
 public abstract class Item {
+
+    /**
+     */
+    @NotNull
+    @OneToOne
+    private Product product;
 }

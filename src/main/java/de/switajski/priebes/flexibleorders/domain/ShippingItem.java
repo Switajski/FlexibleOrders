68b9de429aa21422a53b4624f34459878a76691d
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field boolean --fieldName transmitToSupplier --notNull --class ~.domain.ShippingItem

--- src/main/java/de/switajski/priebes/flexibleorders/domain/ShippingItem.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/ShippingItem.java
@@ -2,9 +2,15 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 public class ShippingItem extends Item {
+
+    /**
+     */
+    @NotNull
+    private Boolean transmitToSupplier;
 }

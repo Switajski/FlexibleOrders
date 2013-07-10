32e22347ecd00f49ba3926fdca8237e00c63953e
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.Item --abstract --inheritanceType TABLE_PER_CLASS --activeRecord false

---
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/Item.java
@@ -0,0 +1,10 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaEntity(inheritanceType = "TABLE_PER_CLASS")
+public abstract class Item {
+}

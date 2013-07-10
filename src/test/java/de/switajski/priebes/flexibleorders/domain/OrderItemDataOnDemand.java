84b8a1be1f25bc8933b2f7101b0e35ba473c1a03
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.OrderItem --extends ~.domain.Item --activeRecord false --testAutomatically

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/OrderItemDataOnDemand.java
@@ -0,0 +1,6 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = OrderItem.class)
+public class OrderItemDataOnDemand {
+}
---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/OrderItemIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = OrderItem.class)
+public class OrderItemIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}

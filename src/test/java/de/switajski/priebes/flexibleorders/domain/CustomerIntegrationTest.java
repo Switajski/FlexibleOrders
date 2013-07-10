a5397c3e38dafbd3c6b20d0897459d38a90c408f
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.Customer --activeRecord false --testAutomatically

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/CustomerIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Customer.class)
+public class CustomerIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}

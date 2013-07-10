aa5375c8a2c01ef1d055c0d6ae2dbc0fc8f7e6f1
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.Category --activeRecord false --testAutomatically

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/CategoryDataOnDemand.java
@@ -0,0 +1,6 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Category.class)
+public class CategoryDataOnDemand {
+}

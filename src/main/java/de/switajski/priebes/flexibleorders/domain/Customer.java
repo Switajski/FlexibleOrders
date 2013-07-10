49d7621b77ed86fc3d80595be0a7aea1d1c6e126
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field string --fieldName shortName --unique --class ~.domain.Customer

--- src/main/java/de/switajski/priebes/flexibleorders/domain/Customer.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/Customer.java
@@ -2,9 +2,15 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.Column;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 public class Customer {
+
+    /**
+     */
+    @Column(unique = true)
+    private String shortName;
 }

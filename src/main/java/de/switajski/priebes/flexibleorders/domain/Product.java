3e1790f7cdc8624eee8d07e54c32a8f599814816
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field number --fieldName productNumber --notNull --unique --type Java.lang.long --class ~.domain.Product

--- src/main/java/de/switajski/priebes/flexibleorders/domain/Product.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/Product.java
@@ -2,9 +2,17 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 public class Product {
+
+    /**
+     */
+    @NotNull
+    @Column(unique = true)
+    private Long productNumber;
 }

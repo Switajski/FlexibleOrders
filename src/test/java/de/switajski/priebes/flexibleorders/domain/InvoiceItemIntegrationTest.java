4ddd2b735e46735f6a8d445f7364d57a1d563a3e
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    entity jpa --class ~.domain.InvoiceItem --extends ~.domain.Item --activeRecord false --testAutomatically

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/InvoiceItemIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = InvoiceItem.class)
+public class InvoiceItemIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}

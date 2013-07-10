58eee97483dac19fecd39201db03828b6ace3648
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field string --fieldName name --notNull --unique --class ~.domain.Category

--- src/main/java/de/switajski/priebes/flexibleorders/domain/Category.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/Category.java
@@ -2,9 +2,17 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 public class Category {
+
+    /**
+     */
+    @NotNull
+    @Column(unique = true)
+    private String name;
 }

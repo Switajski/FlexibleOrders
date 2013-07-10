8cb01cf8d196575a1a758d2623e452555a65909e
Roo Git Add-On <s2-roo@vmware.com>
2013-07-10 14:15:29 +0200

    field date --fieldName expectedDelivery --type java.util.Date --notNull --class ~.domain.ArchiveItem

--- src/main/java/de/switajski/priebes/flexibleorders/domain/ArchiveItem.java
+++ src/main/java/de/switajski/priebes/flexibleorders/domain/ArchiveItem.java
@@ -2,9 +2,21 @@ package de.switajski.priebes.flexibleorders.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 public class ArchiveItem extends Item {
+
+    /**
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date expectedDelivery;
 }

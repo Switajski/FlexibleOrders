78a7fb99b303e9df3b85b5e8de9fa8093e5b34a6
Roo Git Add-On <s2-roo@vmware.com>
2013-09-01 19:01:42 +0200

    confirm OrderItem page added

--- src/main/java/de/switajski/priebes/flexibleorders/web/OrderItemController.java
+++ src/main/java/de/switajski/priebes/flexibleorders/web/OrderItemController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -97,5 +98,20 @@ public class OrderItemController extends JsonController<OrderItem>{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	@RequestMapping(value = "confirm", produces = "text/html")
+    public String confirm(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("orderitems", orderItemService.findOrderItemEntries(firstResult, sizeNo));
+            float nrOfPages = (float) orderItemService.countAllOrderItems() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("orderitems", orderItemService.findAllOrderItems());
+        }
+        addDateTimeFormatPatterns(uiModel);
+        return "orderitems/confirm";
+    }
 
 }

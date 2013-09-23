4d6e3092f0107ad7f207ed7cef309214ffafb6d4
Roo Git Add-On <s2-roo@vmware.com>
2013-09-23 20:12:12 +0200

    test stub --class ~.domain.OrderItem

---
+++ src/test/java/de/switajski/priebes/flexibleorders/domain/OrderItemTest.java
@@ -0,0 +1,107 @@
+package de.switajski.priebes.flexibleorders.domain;
+import org.junit.Test;
+
+public class OrderItemTest {
+
+    private OrderItem orderItem = new OrderItem();
+
+    @Test
+    public void getQuantity() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setQuantity() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getPriceNet() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setPriceNet() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getProductName() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setProductName() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getProductNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setProductNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getOrderConfirmationNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getInvoiceNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getAccountNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getOrderNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getId() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setId() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getVersion() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setVersion() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getOrderItemNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setOrderItemNumber() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getQuantityLeft() {
+        org.junit.Assert.assertTrue(true);
+    }
+
+    @Test
+    public void setQuantityLeft() {
+        org.junit.Assert.assertTrue(true);
+    }
+}

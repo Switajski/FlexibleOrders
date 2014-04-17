a3467c18f47432ed1c24a7176621663959424dd2
Marek <marek@switajski.de>
2014-04-17 22:38:09 +0200

    code formatter and AmountCalculatorTest added

---
+++ src/test/java/de/switajski/priebes/flexibleorders/application/AmountCalculatorTest.java
@@ -0,0 +1,41 @@
+package de.switajski.priebes.flexibleorders.application;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.switajski.priebes.flexibleorders.domain.Amount;
+import de.switajski.priebes.flexibleorders.reference.Currency;
+
+public class AmountCalculatorTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void sum_shouldThrowExceptionWhenAddingDifferentCurrencies() {
+		// GIVEN
+		List<Amount> amounts = Arrays.asList(
+				new Amount(BigDecimal.TEN, Currency.EUR),
+				new Amount(BigDecimal.TEN, Currency.PLN));
+
+		// WHEN
+		AmountCalculator.sum(amounts);
+	}
+
+	@Test
+	public void sum_shouldReturnSum() {
+		// GIVEN
+		List<Amount> amounts = Arrays.asList(
+				new Amount(BigDecimal.TEN, Currency.EUR),
+				new Amount(BigDecimal.TEN, Currency.EUR));
+
+		// WHEN
+		Amount sum = AmountCalculator.sum(amounts);
+
+		// THEN
+		assertThat(sum, equalTo(new Amount(new BigDecimal(20), Currency.EUR)));
+	}
+}

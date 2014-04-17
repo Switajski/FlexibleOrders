61c56c83a30c3eb753ab8fc24f5c29cb8c7197da
Marek <marek@switajski.de>
2014-04-17 22:37:05 +0200

    Code formatting applied, package structure reorganized and some major refactorings.

--- src/main/java/de/switajski/priebes/flexibleorders/policy/OrderPolicy.java
+++
@@ -1,9 +0,0 @@
-package de.switajski.priebes.flexibleorders.policy;
-
-import de.switajski.priebes.flexibleorders.domain.Customer;
-
-public interface OrderPolicy {
-
-	public boolean isAllowed(Customer customer);
-	
-}

863983044009e989837ff72e17808c3739685cc0
Marek <marek@switajski.de>
2014-04-18 13:58:51 +0200

    fixed: pdf-view.xml suffered under refactoring

---
+++ src/test/java/de/switajski/priebes/flexibleorders/testhelper/UnitTestSuite.java
@@ -0,0 +1,15 @@
+package de.switajski.priebes.flexibleorders.testhelper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.switajski.priebes.flexibleorders.application.AmountCalculatorTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		AmountCalculatorTest.class
+})
+public class UnitTestSuite {
+
+}

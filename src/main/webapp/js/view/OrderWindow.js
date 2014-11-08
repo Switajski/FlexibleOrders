1e8fb29a45380fdfabae41cd16dd63071f2faf90
Marek <marek@switajski.de>
2014-11-08 16:15:53 +0100

    Order Window redesigned

--- src/main/webapp/js/view/OrderWindow.js
+++ src/main/webapp/js/view/OrderWindow.js
@@ -5,27 +5,44 @@ Ext.define('MyApp.view.OrderWindow', {
 	id : 'OrderWindow',
 	headerForm : {
 		xtype : 'fieldset',
-		// title : 'Kunde',
+		title : 'Kunde',
 		flex : 1,
 		items : [{
+		    xtype: 'fieldcontainer',
+            layout: 'hbox',
+            combineErrors: true,
+            defaultType: 'textfield',
+            defaults : {
+            	labelWidth : 70
+            },
+			items : [{
 					xtype : 'displayfield',
 					anchor : '100%',
 					name : 'customerNumber',
-					fieldLabel : 'Kundennr'
+					fieldLabel : 'Kundennr',
 				}, {
 					xtype : 'displayfield',
 					anchor : '100%',
-					name : 'lastName',
-					fieldLabel : 'Nachname'
-				}, {
-					xtype : 'datefield',
-					format : 'd/m/Y',
-					allowBlank : true,
-					fieldLabel : 'Bestelldatum',
-					name : 'created'
-				}, {
+					margins: '0 0 0 6',
+					name : 'firstName'
+				},{
+					xtype : 'displayfield',
+					anchor : '100%',
+					margins: '0 0 0 6',
+					name : 'lastName'
+				}],
+		},{
+			xtype: 'fieldcontainer',
+		    layout: 'hbox',
+		    combineErrors: true,
+		    defaultType: 'textfield',
+		    defaults : {
+            	labelWidth : 70
+            },
+		    items : [{
 					itemid : 'newOrderNumber',
 					xtype : 'ordernumbercombobox',
+					labelWidth : 60,
 					fieldLabel : 'Bestellnr',
 					listeners : {
 						// change : this.onOrderNumberChange,
@@ -38,7 +55,15 @@ Ext.define('MyApp.view.OrderWindow', {
 							}
 						}
 					}
+				},{
+					xtype : 'datefield',
+					format : 'd/m/Y',
+					allowBlank : true,
+					fieldLabel : 'Bestelldatum',
+					margins: '0 0 0 10',
+					name : 'created'
 				}]
+		}]
 	},
 	deliveryAddressForm : null,
 	addressForm : null,

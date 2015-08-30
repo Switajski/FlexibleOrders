8519d598402b6a68c67ee412326433bb3ef06d7f
Marek <marek@switajski.de>
2015-08-30 13:55:15 +0200

    Added testsuite in order to see changes on all PDF files.

---
+++ src/test/java/de/switajski/priebes/flexibleorders/itextpdf/PdfFilesUnitTestSuite.java
@@ -0,0 +1,15 @@
+package de.switajski.priebes.flexibleorders.itextpdf;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    DeliveryNotesPdfFileTest.class,
+    InvoicePdfFileTest.class,
+    OrderConfirmationPdfFileTest.class,
+    OrderPdfFileTest.class
+})
+public class PdfFilesUnitTestSuite {
+
+}

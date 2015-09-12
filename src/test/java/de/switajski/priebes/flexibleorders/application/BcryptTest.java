aeeb687d74a0c667ef61b553bd8e61114968f51d
Marek <marek@switajski.de>
2015-09-12 17:31:50 +0200

    Password encoder added

---
+++ src/test/java/de/switajski/priebes/flexibleorders/application/BcryptTest.java
@@ -0,0 +1,15 @@
+package de.switajski.priebes.flexibleorders.application;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptTest {
+
+    @Test
+    public void startEncryption(){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode("00TAY9fci5V");
+        
+        System.out.println(hashedPassword);
+    }
+}

90364483c8e268b0ce7c715131058791fede5312
Marek <marek@switajski.de>
2015-09-17 22:12:41 +0200

    JavaScript GUI removed

--- src/main/webapp/js/store/InvoiceItemDataStore.js
+++
@@ -1,7 +0,0 @@
-Ext.define('MyApp.store.InvoiceItemDataStore', {
-	extend : 'MyApp.store.ItemDataStore',
-	alias : 'widget.InvoiceItemDataStore',
-	customurl : '/FlexibleOrders/reportitems/listAllToBeProcessed',
-	customstoreid : 'InvoiceItemDataStore',
-	groupField : 'documentNumber'
-});

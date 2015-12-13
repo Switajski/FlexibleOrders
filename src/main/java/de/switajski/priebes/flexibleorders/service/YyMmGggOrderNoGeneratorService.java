1d6b48f55a6c929cfbf3b50595dce8ed2614a89a
Marek <marek@switajski.de>
2015-12-13 20:16:40 +0100

    Bug fixed: generation of next order number was dependent from the numbers of all orders. This strategy failed, when one order in between was deleted.

--- src/main/java/de/switajski/priebes/flexibleorders/service/YyMmGggOrderNoGeneratorService.java
+++ src/main/java/de/switajski/priebes/flexibleorders/service/YyMmGggOrderNoGeneratorService.java
@@ -1,11 +1,19 @@
 package de.switajski.priebes.flexibleorders.service;
 
+import static org.springframework.data.jpa.domain.Specifications.where;
+
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import de.switajski.priebes.flexibleorders.domain.Order;
 import de.switajski.priebes.flexibleorders.repository.OrderRepository;
 import de.switajski.priebes.flexibleorders.repository.specification.OrderCreatedBetweenSpecification;
+import de.switajski.priebes.flexibleorders.repository.specification.OrderNumberStartsWith;
 import de.switajski.priebes.flexibleorders.service.api.OrderNumberGeneratorService;
 
 @Service
@@ -18,13 +26,25 @@ public class YyMmGggOrderNoGeneratorService implements OrderNumberGeneratorServi
     public String generate(LocalDate date) {
         Integer year = date.getYear();
         Integer month = date.getMonthOfYear();
-        Long generated = orderRepo.count(OrderCreatedBetweenSpecification.inMonth(date));
-        generated = generated + 1;
+        Specification<Order> spec =
+                where(OrderCreatedBetweenSpecification.inMonth(date))
+                        .and(new OrderNumberStartsWith("B"));
+
+        int lastOrderNumber = 0;
+        Page<Order> orders = orderRepo.findAll(spec, new PageRequest(0, 1, Sort.Direction.DESC, "orderNumber"));
+        if (0 < orders.getContent().size()) {
+            String numbers = orders.iterator().next().getOrderNumber().replaceAll("[\\D]", "");
+            String lastThreeNumbers = numbers.substring(Math.max(0, numbers.length() - 3));
+            lastOrderNumber = Integer.parseInt(lastThreeNumbers);
+        }
+
+        int generated = lastOrderNumber + 1;
+
         return new StringBuilder()
                 .append("B")
                 .append(year.toString().substring(2))
                 .append(("00" + month).substring(month.toString().length()))
-                .append(("000" + generated).substring(generated.toString().length())).toString();
+                .append(("000" + generated).substring(new Integer(generated).toString().length())).toString();
     }
 
 }
--- src/test/java/de/switajski/priebes/flexibleorders/service/api/OrderNumberGeneratorServiceTest.java
+++ src/test/java/de/switajski/priebes/flexibleorders/service/api/OrderNumberGeneratorServiceTest.java
@@ -1,22 +1,27 @@
 package de.switajski.priebes.flexibleorders.service.api;
 
 import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+
 import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
+import de.switajski.priebes.flexibleorders.domain.Order;
 import de.switajski.priebes.flexibleorders.repository.OrderRepository;
 import de.switajski.priebes.flexibleorders.service.YyMmGggOrderNoGeneratorService;
+import de.switajski.priebes.flexibleorders.testhelper.EntityBuilder.OrderBuilder;
 
 public class OrderNumberGeneratorServiceTest {
 
@@ -32,10 +37,14 @@ public class OrderNumberGeneratorServiceTest {
         // GIVEN
         String year = "15", month = "01";
         String randomDay = "12";
-        String noOfSavedOrders = "065";
+        String lastSavedOrder = "065";
         String consecutiveNo = "066";
         MockitoAnnotations.initMocks(this);
-        when(orderRepo.count(any(Specification.class))).thenReturn(parseLong(noOfSavedOrders));
+        when(orderRepo.findAll(any(Specification.class), any(Pageable.class))).thenReturn(
+                new PageImpl<Order>(Arrays.asList(
+                        new OrderBuilder().setOrderNumber(lastSavedOrder).build()
+                        ))
+                );
 
         // WHEN
         String orderNumber = orderNumberGeneratorService.generate(new LocalDate(parseInt("20" + year), parseInt(month), parseInt(randomDay)));

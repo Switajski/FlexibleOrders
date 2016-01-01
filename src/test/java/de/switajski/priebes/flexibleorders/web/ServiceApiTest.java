b4e5583a74c3654395b5a9125e3b8cdb0310a8b9
Marek Switajski <marek@Switajski.de>
2016-01-01 16:57:51 +0100

    removed duplicated code

--- src/test/java/de/switajski/priebes/flexibleorders/web/ServiceApiTest.java
+++ src/test/java/de/switajski/priebes/flexibleorders/web/ServiceApiTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.switajski.priebes.flexibleorders.web.dto.CustomerDto;
@@ -17,14 +18,13 @@ public class ServiceApiTest extends SpringMvcTestConfiguration {
 
     @Test
     public void shouldBeRejectedByValidation() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
         JsonCreateReportRequest request = new JsonCreateReportRequest();
         request.setCustomerId(45L);
 
         mvc.perform(post("/transitions/order")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request)))
+                .content(createStringRequest(request)))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().string(containsString("\"errors\":{\"items\":")))
                 .andExpect(status().is4xxClientError());
@@ -32,18 +32,22 @@ public class ServiceApiTest extends SpringMvcTestConfiguration {
 
     @Test
     public void shouldValidateCustomer() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
         CustomerDto customer = new CustomerDto();
         customer.customerNumber = 123L;
 
         mvc.perform(post("/customers/create")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(customer)))
+                .content(createStringRequest(customer)))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().string(containsString("errors")))
                 .andExpect(content().string(containsString("companyName")))
                 .andExpect(status().is4xxClientError());
 
+    }
+
+    private String createStringRequest(Object o) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(o);
     }
 }

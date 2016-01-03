ea19104a8d16f0b7a85162af6ca8a98da6ae4631
Marek Switajski <marek@Switajski.de>
2016-01-03 16:02:39 +0100

    #103 Quantity validation set to deprecated.

--- src/main/java/de/switajski/priebes/flexibleorders/service/QuantityUtility.java
+++ src/main/java/de/switajski/priebes/flexibleorders/service/QuantityUtility.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import de.switajski.priebes.flexibleorders.domain.report.ReportItem;
 import de.switajski.priebes.flexibleorders.itextpdf.builder.Unicode;
+import de.switajski.priebes.flexibleorders.validation.UniqueDocumentNumber;
 
 public class QuantityUtility {
 
@@ -17,7 +18,7 @@ public class QuantityUtility {
 
     // TODO: move to validator
     /**
-     *
+     * @deprecated use custom validator like @link {@link UniqueDocumentNumber}
      * @param qty
      *            to bring in the next process step
      * @param reportItem
@@ -33,8 +34,12 @@ public class QuantityUtility {
                 " hat keine offenen Positionen mehr");
 
         if (qty > toBeProcessed) {
-            StringBuilder msg = new StringBuilder("angeforderte Menge ist ").append(qty).append(". ")
-                    .append(reportItem.overdue()).append(" sind aber noch ").append(Unicode.U_UML).append("brig.");
+            StringBuilder msg = new StringBuilder("angeforderte Menge ist ").append(qty)
+                    .append(". ")
+                    .append(reportItem.overdue())
+                    .append(" sind aber noch ")
+                    .append(Unicode.U_UML)
+                    .append("brig.");
             throw new IllegalArgumentException(msg.toString());
         }
     }

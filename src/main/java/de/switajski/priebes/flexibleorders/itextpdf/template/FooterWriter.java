19b8612713fcc145251aaf1fff1875949bd1f1cf
Marek Switajski <marek@Switajski.de>
2016-02-23 21:39:16 +0100

    TODOs removed

--- src/main/java/de/switajski/priebes/flexibleorders/itextpdf/template/FooterWriter.java
+++ src/main/java/de/switajski/priebes/flexibleorders/itextpdf/template/FooterWriter.java
@@ -15,7 +15,6 @@ public class FooterWriter implements PdfPageEvent {
 
     @Override
     public void onOpenDocument(PdfWriter writer, Document document) {
-        // TODO Auto-generated method stub
 
     }
 
@@ -54,55 +53,46 @@ public class FooterWriter implements PdfPageEvent {
 
     @Override
     public void onEndPage(PdfWriter writer, Document document) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onCloseDocument(PdfWriter writer, Document document) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onParagraph(PdfWriter writer, Document document, float paragraphPosition) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onChapter(PdfWriter writer, Document document, float paragraphPosition, Paragraph title) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onChapterEnd(PdfWriter writer, Document document, float paragraphPosition) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onSection(PdfWriter writer, Document document, float paragraphPosition, int depth, Paragraph title) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onSectionEnd(PdfWriter writer, Document document, float paragraphPosition) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onGenericTag(PdfWriter writer, Document document, Rectangle rect, String text) {
-        // TODO Auto-generated method stub
 
     }
 

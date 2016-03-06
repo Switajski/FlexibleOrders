eaa638c24579efe585fb411159155e4c8652d3cd
Marek Switajski <marek@Switajski.de>
2016-03-06 13:32:51 +0100

    #103 Added test configuration, that is able to inject mocks into @Component annotated validators.

--- src/test/java/de/switajski/priebes/flexibleorders/validation/ValidationStaticTestConfiguration.java
+++ src/test/java/de/switajski/priebes/flexibleorders/validation/ValidationStaticTestConfiguration.java
@@ -1,27 +1,57 @@
 package de.switajski.priebes.flexibleorders.validation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Configuration;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorFactory;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
-import org.junit.Before;
+import org.hibernate.validator.internal.util.ReflectionHelper;
 
 /**
  * Static, because the validator is initialized statically. FlexibleOrders also
- * uses validators with dependencies, that are injected by spring (e.g.
- * {@link ReportNumberValidator}).
+ * uses validators with '@Component' annotation, that are injected by spring
+ * (e.g. {@link ReportNumberValidator}). In order to mock those dependencies use
+ * this test configuration and inject mocks by
+ * {@link #inject(Class, ConstraintValidator)}
  * 
  * @author switajski
  *
  */
 public abstract class ValidationStaticTestConfiguration {
 
-    static Validator validator;
+    Validator validator;
 
-    @Before
-    public void setup() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+    Map<Class<?>, ConstraintValidator<?, ?>> constraintValidators = new HashMap<>();
+
+    private void setup() {
+        Configuration<?> configuration = Validation.byDefaultProvider().configure();
+        configuration.constraintValidatorFactory(new ConstraintValidatorDelegate(constraintValidators));
+
+        validator = configuration.buildValidatorFactory().getValidator();
+    }
+
+    protected void inject(Class<?> clazz, ConstraintValidator<?, ?> cValidator) {
+        constraintValidators.put(clazz, cValidator);
+        setup();
+    }
+
+    private class ConstraintValidatorDelegate implements ConstraintValidatorFactory {
+
+        private Map<Class<?>, ConstraintValidator<?, ?>> validators;
+
+        public ConstraintValidatorDelegate(Map<Class<?>, ConstraintValidator<?, ?>> constraintValidators) {
+            validators = constraintValidators;
+        }
+
+        public final <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
+            if (validators.containsKey(key)) return (T) validators.get(key);
+            return ReflectionHelper.newInstance(key, "ConstraintValidator");
+        }
+
     }
 
 }

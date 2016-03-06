5db3b7a567d4d93f5f2a97eb55072324a5ebca2d
Marek Switajski <marek@Switajski.de>
2016-03-06 13:55:45 +0100

    #103 Changed test to use new unit test configuration for testing validators with injected beans.

--- src/test/java/de/switajski/priebes/flexibleorders/validation/DeliverParameterValidationTest.java
+++ src/test/java/de/switajski/priebes/flexibleorders/validation/DeliverParameterValidationTest.java
@@ -2,30 +2,44 @@ package de.switajski.priebes.flexibleorders.validation;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import de.switajski.priebes.flexibleorders.domain.report.DeliveryNotes;
+import de.switajski.priebes.flexibleorders.repository.ReportRepository;
 import de.switajski.priebes.flexibleorders.service.process.parameter.DeliverParameter;
-import de.switajski.priebes.flexibleorders.testconfiguration.SpringMvcWithTestDataTestConfiguration;
 
-public class DeliverParameterValidationTest extends SpringMvcWithTestDataTestConfiguration {
-
-    @Autowired
-    Validator validator;
+public class DeliverParameterValidationTest extends ValidationStaticTestConfiguration {
 
     Set<ConstraintViolation<DeliverParameter>> dpConstraintViolations;
     DeliverParameter deliverParameter = new DeliverParameter();
 
+    @Mock
+    ReportRepository reportRepository;
+    @InjectMocks
+    ReportNumberValidator reportNumberValidator = new ReportNumberValidator();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        inject(ReportNumberValidator.class, reportNumberValidator);
+    }
+
     @Test
     public void shouldNotAcceptDuplicateDocumentNumber() {
         deliverParameter.setDeliveryNotesNumber("L11");
+        when(reportRepository.findByDocumentNumber(deliverParameter.getDeliveryNotesNumber()))
+                .thenReturn(new DeliveryNotes());
 
         whenValidatingDeliverParameter();
 

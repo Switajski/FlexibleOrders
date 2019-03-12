6718444d719ef355896ce0fff068ef7a8b717b73
Marek <marek@switajski.de>
2019-03-12 07:51:01 +0100

    remove dropbox integration and security vulnerabilities

--- src/test/java/de/switajski/priebes/flexibleorders/testconfiguration/SpringMvcTestConfiguration.java
+++ src/test/java/de/switajski/priebes/flexibleorders/testconfiguration/SpringMvcTestConfiguration.java
@@ -3,7 +3,6 @@ package de.switajski.priebes.flexibleorders.testconfiguration;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -19,16 +18,12 @@ public class SpringMvcTestConfiguration {
     @Autowired
     WebApplicationContext wac;
 
-    @Autowired
-    FilterChainProxy springSecurityFilter;
-
     protected MockMvc mvcWithSecurity;
 
     protected MockMvc mvc;
 
     @Before
     public void setup() {
-        mvcWithSecurity = MockMvcBuilders.webAppContextSetup(wac).addFilter(springSecurityFilter, "/*").build();
         mvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 

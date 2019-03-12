b3b634f011d0c4bc159ab3374412e71823821589
Marek <marek@switajski.de>
2019-03-12 07:44:42 +0100

    make backend APIs work

--- src/main/java/de/switajski/priebes/flexibleorders/SpringBootApplication.java
+++ src/main/java/de/switajski/priebes/flexibleorders/SpringBootApplication.java
@@ -2,8 +2,12 @@ package de.switajski.priebes.flexibleorders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class SpringBootApplication {
   
   public static void main(String[] args) {
